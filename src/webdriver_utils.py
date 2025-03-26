6497a54f0e073bee8e8f29f01427433c34f06304
abhinav-pandey29 <abhinav_pandey29@yahoo.com>
2025-03-27 02:54:34 +0530

    feat(webdriver): deny permission prompts in webdrivers

--- src/webdriver_utils.py
+++ src/webdriver_utils.py
@@ -6,6 +6,7 @@ from seleniumwire.undetected_chromedriver.v2 import Chrome, ChromeOptions
 def initialize_driver(headless=False, implicit_wait: int = None):
     options = uc.ChromeOptions()
     options.add_argument("--log-level=3")
+    options.add_argument("--deny-permission-prompts")
     if headless:
         options.add_argument("--headless")
 
@@ -13,13 +14,13 @@ def initialize_driver(headless=False, implicit_wait: int = None):
     driver = uc.Chrome(options=options, driver_executable_path=executable_path)
     if implicit_wait and isinstance(implicit_wait, (int, float)):
         driver.implicitly_wait(implicit_wait)
-
     return driver
 
 
 def init_seleniumwire_webdriver(*args):
     chrome_options = ChromeOptions()
     chrome_options.add_argument("--log-level=3")
+    chrome_options.add_argument("--deny-permission-prompts")
     chrome_options.add_argument("--ignore-certificate-errors")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-gpu")
